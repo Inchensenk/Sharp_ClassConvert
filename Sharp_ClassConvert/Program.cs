95f3eff97a010978deb1986123e93de3f5a96097
sofoyan gevork <aab8117@gmail.com>
2022-04-01 11:42:41 +0300

    Добавьте файлы проекта.

---
+++ Sharp_ClassConvert/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Sharp_ClassConvert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int a, b;
+           
+
+            Console.WriteLine("Введите число 1");
+
+            //сохранение считанной с консоли строки в переменную
+            str = Console.ReadLine();
+
+            //конвертация введенной строки в целочисленный тип и присваивание сконвертированного значения переменной a
+            a= Convert.ToInt32(str);
+
+            Console.WriteLine("Ведите число 2");
+
+            //сохранение считанной с консоли строки в переменную
+            str = Console.ReadLine();
+
+            //конвертация введенной строки в целочисленный тип и присваивание сконвертированного значения переменной b
+            b = Convert.ToInt32(str);
+
+            int result = a + b;
+
+            Console.WriteLine($"Сумма чисел = {result}");
+
+            Console.WriteLine("Ведите дробное число 1 (с разделитем /)");
+
+            str = Console.ReadLine();
+
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                //присваиваем полю NumberDecimalSeparator символ разделителя который будем использовать привводе дробного числа в консоль(символ может быть любой)
+                NumberDecimalSeparator = "/",
+            };
+
+            //вторым параметром указываем numberFormatInfo, который хранит ранее указанный разделитель дробного числа
+            double c = Convert.ToDouble(str, numberFormatInfo);
+
+            Console.WriteLine("Ведите дробное число 2 (с разделитем /)");
+
+            str = Console.ReadLine();
+
+            double d = Convert.ToDouble(str, numberFormatInfo);
+
+            double result2 = c + d;
+
+            Console.WriteLine($"Сумма чисел = {result2}");
+        }
+    }
+}
---
+++ Sharp_ClassConvert/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Sharp_ClassConvert")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sharp_ClassConvert")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("de4dc7a6-d0ca-4ea9-860a-0378c1a84eaf")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
